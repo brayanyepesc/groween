457e0d44f8439361a37d3cf60f61d29f5479058d
Brayan Yepes <abrayan.claye@gmail.com>
2024-06-27 10:30:39 -0500

    fix: id autoicrement disabled

--- src/app/platform/profiles/[id]/page.tsx
+++ src/app/platform/profiles/[id]/page.tsx
@@ -5,13 +5,7 @@ import { NextPublicApiUrl } from "@/shared/env";
 
 async function getData(id: string) {
     try {
-        const res = await axios.get(`${NextPublicApiUrl}/api/recyclers/${id}`, {
-            headers: {
-                'Cache-Control': 'no-cache',
-                'Pragma': 'no-cache',
-                'Expires': '0',
-            }
-        });
+        const res = await axios.get(`${NextPublicApiUrl}/api/recyclers/${id}`);
         if (res.status === 200) {
             return res.data;
         } else {
